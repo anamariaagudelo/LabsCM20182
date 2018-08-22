ca4a096c1513da33f5830767d19a334002102e6d
Administrador <anamariaagudeloisigmail.com>
2018-08-22 02:02:48 -0500

    finalizacion de splash screen

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20182/lab1/splashscreen.java
@@ -0,0 +1,43 @@
+package co.edu.udea.compumovil.gr03_20182.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.*;
+import android.app.*;
+import android.content.*;
+import android.content.pm.ActivityInfo;
+import android.view.*;
+import android.widget.*;
+import android.os.*;
+
+public class splashscreen extends Activity {
+    private Timer tiempo;
+    private ProgressBar barraProgreso;
+    private int i=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        barraProgreso = (ProgressBar) findViewById(R.id.barraProgreso);
+        barraProgreso.setProgress(0);
+        final long intervalo = 45;
+        tiempo = new Timer();
+        tiempo.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(i < 100){
+                    barraProgreso.setProgress(i);
+                    i++;
+                }else{
+                    tiempo.cancel();
+                    Intent intent = new Intent().setClass(
+                            splashscreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        },0,intervalo);
+
+    }
+}
