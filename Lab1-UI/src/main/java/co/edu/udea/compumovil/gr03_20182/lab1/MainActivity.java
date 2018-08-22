acedb7dc080eae2477ababcccb6c399459809954
sarra232 <41738408+sarra232@users.noreply.github.com>
2018-08-22 16:18:52 -0500

    Punto B y comienzo del punto C. Wilson Sarrazola

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20182/lab1/MainActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20182/lab1/MainActivity.java
@@ -1,13 +1,34 @@
 package co.edu.udea.compumovil.gr03_20182.lab1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button comida;
+    Button bebida;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        comida = (Button)findViewById(R.id.botonComida);
+        comida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent comida = new Intent(MainActivity.this, comidaActivity.class);
+                startActivity(comida);
+            }
+        });
+        bebida = (Button)findViewById(R.id.botonBebida);
+        bebida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bebida = new Intent(MainActivity.this, bebidaActivity.class);
+                startActivity(bebida);
+            }
+        });
     }
 }
